c155ecdb6db6b7bcf88ec86ca0a853d286b12a72
Arfi Renaldi <arfirenaldi@gmail.com>
2022-07-16 19:24:56 +0700

    add Home container

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -13,9 +13,8 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
 import FeaturePage from 'containers/FeaturePage/Loadable';
+import Home from 'containers/Home';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
-import Header from 'components/Header';
-import Footer from 'components/Footer';
 
 import GlobalStyle from '../../global-styles';
 
@@ -31,19 +30,15 @@ const AppWrapper = styled.div`
 export default function App() {
   return (
     <AppWrapper>
-      <Helmet
-        titleTemplate="%s - React.js Boilerplate"
-        defaultTitle="React.js Boilerplate"
-      >
-        <meta name="description" content="A React.js Boilerplate application" />
+      <Helmet>
+        <meta name="description" content="Randomuser" />
       </Helmet>
-      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/users" component={Home} />
         <Route path="/features" component={FeaturePage} />
         <Route path="" component={NotFoundPage} />
       </Switch>
-      <Footer />
       <GlobalStyle />
     </AppWrapper>
   );
---
+++ app/containers/Home/index.js
@@ -0,0 +1,52 @@
+/**
+ *
+ * Home
+ *
+ */
+
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import { useInjectSaga } from 'utils/injectSaga';
+import { useInjectReducer } from 'utils/injectReducer';
+import makeSelectHome from './selectors';
+import reducer from './reducer';
+import saga from './saga';
+
+export function Home() {
+  useInjectReducer({ key: 'home', reducer });
+  useInjectSaga({ key: 'home', saga });
+
+  return (
+    <div>
+      Home container
+    </div>
+  );
+}
+
+Home.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  home: makeSelectHome(),
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+export default compose(
+  withConnect,
+  memo,
+)(Home);
